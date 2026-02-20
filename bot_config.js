48000eb0ab3d91bb02529ca0a9f5e147f85086ae
D.S.S.KARUNARATHNA <mrdarkhyperlegend@gmail.com>
2026-02-20 13:34:54 +0530

    GPT-AI 💗

--- bot_config.js
+++ bot_config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    openai_api_key: 'ඔබේ_OPENAI_API_KEY_එක_මෙහි_යොදන්න',
-    prefix: '!', // Bot එකට විධානයන් දීමට භාවිතා කරන ලකුණ (උදා: !hello)
+    openai_api_key: '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',
+    prefix: '.', // Bot එකට විධානයන් දීමට භාවිතා කරන ලකුණ (උදා: !hello)
 };
